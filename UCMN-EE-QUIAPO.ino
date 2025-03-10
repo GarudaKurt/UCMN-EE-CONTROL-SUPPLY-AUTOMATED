5731eef667cf14ef40f9be7b88749be031a70eb9
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-03-10 16:05:36 +0800

    FINAL CODE

--- UCMN-EE-QUIAPO.ino
+++ UCMN-EE-QUIAPO.ino
@@ -4,6 +4,11 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+#define SS_PIN 53
+#define RST_PIN 5
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);  // 20x4 LCD
 
@@ -24,29 +29,22 @@ Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 int relayPins[8] = {30, 31, 32, 33, 34, 35, 36, 37};
 bool relayStates[8] = {false, false, false, false, false, false, false, false};
 
+// List of valid RFIDs
+String validRFIDs[] = {
+  "F3 5C 32 22", "34 99 2C F9", "34 67 2B F9", 
+  "F4 73 4F DF", "44 7F 28 DB", "24 9F 60 DF", "54 B8 40 DB"
+};
 
-// show main menu
-void showMenu() {
-  lcd.clear();
-  lcd.setCursor(2, 0);
-  lcd.print("[1] Individual");
-  lcd.setCursor(2, 1);
-  lcd.print("[2] Group");
-  lcd.setCursor(2, 2);
-  lcd.print("[3] Multiple");
-  lcd.setCursor(2, 3);
-  lcd.print("[4] Reset");
-}
-
-void scanRFIDPrompt() {
-  lcd.clear();
-  lcd.setCursor(2, 0);
-  lcd.print("Scan Your RFID");
-}
+// Function prototypes
+void scanRFIDPrompt();
+bool validateRFID();
+void showMenu();
 
 void setup() {
-  // Initialize LCD
   Serial.begin(115200);
+  SPI.begin();
+  rfid.PCD_Init();
+
   lcd.init(); 
   lcd.backlight();
   lcd.setCursor(3, 0);
@@ -54,27 +52,34 @@ void setup() {
   lcd.setCursor(2, 1);
   lcd.print("System Loading...");
   delay(2000);
-  showMenu();
-  //scanRFIDPrompt();
+  
+  scanRFIDPrompt();  // Start with RFID scanning
 
   for (int i = 0; i < 8; i++) {
     pinMode(relayPins[i], OUTPUT);
-    digitalWrite(relayPins[i], HIGH);  // Relays OFF (IN-ACTIVE LOW) //RELAY ON (ACTIVE HIGH)
+    digitalWrite(relayPins[i], HIGH);  // Relays OFF (Active LOW)
   }
 }
 
 void loop() {
-  char key = keypad.getKey();
-  
-  if (key) {
+  if (!validateRFID()) {
+    return;  // Keep scanning for a valid card
+  }
+
+  showMenu();
+
+  while (true) {
+    char key = keypad.getKey();
+    if (!key) continue;
+
     lcd.clear();
    
     if (key == '1') {  // Individual Light Control
-      Serial.println("Hello");
       lcd.setCursor(0, 0);
       lcd.print("Select Light [1-8]:");
       char lightKey;
       while (!(lightKey = keypad.getKey()));
+
       if (lightKey >= '1' && lightKey <= '8') {
         int relayIndex = lightKey - '1';
         relayStates[relayIndex] = true;
@@ -102,7 +107,7 @@ void loop() {
       lcd.print("Select Lights [1-8]:");
       lcd.setCursor(0, 1);
       lcd.print("Press # to end.");
-      
+
       char lightKey;
       while (1) {
         lightKey = keypad.getKey();
@@ -179,7 +184,56 @@ void loop() {
       showMenu();
     }
    }
+   
     delay(2000);
-    showMenu();
+    scanRFIDPrompt();  // After execution, go back to scanning
+    return;
   }
+}
+
+void showMenu() {
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("[1] Individual");
+  lcd.setCursor(2, 1);
+  lcd.print("[2] Group");
+  lcd.setCursor(2, 2);
+  lcd.print("[3] Multiple");
+  lcd.setCursor(2, 3);
+  lcd.print("[4] Reset");
+}
+
+
+void scanRFIDPrompt() {
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Scan Your RFID");
+}
+
+bool validateRFID() {
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+    return false;
+  }
+  Serial.println("[DEBUG] pass here!");
+  String scannedRFID = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    scannedRFID += String(rfid.uid.uidByte[i], HEX);
+    if (i < rfid.uid.size - 1) scannedRFID += " ";
+  }
+  scannedRFID.toUpperCase();
+
+  for (String validID : validRFIDs) {
+    if (scannedRFID == validID) {
+      Serial.println("RFID Accepted: " + scannedRFID);
+      return true;
+    }
+  }
+
+  Serial.println("RFID Denied: " + scannedRFID);
+  lcd.clear();
+  lcd.setCursor(3, 1);
+  lcd.print("Access Denied!");
+  delay(2000);
+  scanRFIDPrompt();
+  return false;
 }
